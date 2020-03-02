79ae6349cf248695fa09e1b0a6006ff88e1eecbd
Alex Smetukh <alex.smetukh@gmail.com>
2020-03-02 13:12:58 +0200

    add reducer

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Button, FlatList } from 'react-native';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 
-export default function App() {
+import todosReducer from './store/reducers/todos';
+
+const rootReducer = combineReducers({
+  todos: todosReducer
+});
+
+const store = createStore(rootReducer);
+
+function App() {
   const [courseGoals, setCourseGoals] = useState([]);
   const [isAddMode, setIsAddMode] = useState(false);
 
@@ -27,25 +37,28 @@ export default function App() {
   };
 
   return (
-    <View style={styles.screen}>
-      <Button title="Add New Goal" onPress={() => setIsAddMode(true)} />
-      <GoalInput
-        visible={isAddMode}
-        onAddGoal={addGoalHandler}
-        onCancel={cancelGoalAdditionHandler}
-      />
-      <FlatList
-        keyExtractor={(item, index) => item.id}
-        data={courseGoals}
-        renderItem={itemData => (
-          <GoalItem
-            id={itemData.item.id}
-            onDelete={removeGoalHandler}
-            title={itemData.item.value}
-          />
-        )}
-      />
-    </View>
+    <Provider store={store}>
+      <View style={styles.screen}>
+        <Button title="Add New Goal" onPress={() => setIsAddMode(true)} />
+        <GoalInput
+          visible={isAddMode}
+          onAddGoal={addGoalHandler}
+          onCancel={cancelGoalAdditionHandler}
+        />
+        <FlatList
+          keyExtractor={(item, index) => item.id}
+          data={courseGoals}
+          renderItem={itemData => (
+            <GoalItem
+              id={itemData.item.id}
+              onDelete={removeGoalHandler}
+              title={itemData.item.value}
+            />
+          )}
+        />
+      </View>
+    </Provider>
+    
   );
 }
 
@@ -54,3 +67,5 @@ const styles = StyleSheet.create({
     padding: 50
   }
 });
+
+export default App;
